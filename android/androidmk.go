1e8bfcbf4b0747d7aafc962badbed475c9ef8863
Tongbo Liu <liutongbo@google.com>
2024-02-22 17:50:59 +0800

    [DO NOT MERGE] Build MCTS on mainline branches
    Ignore-AOSP-First: test for mainline internal branch
    Bug: 323532076
    Test: m mcts
    Change-Id: I0357c29fdd604c48859f5b5c5bf96e27da41fef6

--- android/androidmk.go
+++ android/androidmk.go
@@ -278,8 +278,8 @@ func (a *AndroidMkEntries) AddStrings(name string, value ...string) {
 // AddCompatibilityTestSuites adds the supplied test suites to the EntryMap, with special handling
 // for partial MTS and MCTS test suites.
 func (a *AndroidMkEntries) AddCompatibilityTestSuites(suites ...string) {
-	// M(C)TS supports a full test suite and partial per-module MTS test suites, with naming mts-${MODULE}.
-	// To reduce repetition, if we find a partial M(C)TS test suite without an full M(C)TS test suite,
+	// MT(C)S supports a full test suite and partial per-module MT(C)S test suites, with naming mts-${MODULE}.
+	// To reduce repetition, if we find a partial MT(C)S test suite without an full MTS test suite,
 	// we add the full test suite to our list.
 	if PrefixInList(suites, "mts-") && !InList("mts", suites) {
 		suites = append(suites, "mts")
